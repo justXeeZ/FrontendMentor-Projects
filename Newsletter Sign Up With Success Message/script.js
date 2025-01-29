e39a474bb53c1075128023184bb9af0973b566fe
justXeeZ <yassin03mr@gmail.com>
2025-01-29 16:47:05 +0200

    improved solution

--- Newsletter Sign Up With Success Message/script.js
+++ Newsletter Sign Up With Success Message/script.js
@@ -2,13 +2,26 @@ const btnSignup = document.getElementById("btn-signup");
 const btnDismiss = document.getElementById("btn-dismiss");
 const card = document.getElementById("card");
 const successMessage = document.getElementById("success-message");
+const email = document.querySelector(".signup-input")
+const main = document.querySelector("main");
+successMessage.remove();
 
 btnSignup.onclick = () => {
-	card.style.display = "none";
-	successMessage.style.display = "flex";
+	if (email.value === ""){
+		email.focus()
+		email.placeholder = "Please enter your email"
+	} else{
+		card.remove();
+		/*
+			I put it after child node 5 to make it appear
+			after the right comment in the DOM
+			(the <!-- Success message --> comment in the index.html)
+		*/
+		main.childNodes[5].after(successMessage);
+	}
 };
 
 btnDismiss.onclick = () => {
-	card.style.display = "flex";
-	successMessage.style.display = "none";
+	successMessage.remove();
+	main.childNodes[1].after(card);
 };
